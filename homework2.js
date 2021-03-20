1aba788a3346fcd5bfc179d4a2b34295748b682f
Ani <anihambardzumyan97@gmail.com>
2021-03-20 14:43:39 +0400

    some changes

--- homework2.js
+++ homework2.js
@@ -1,2 +1,62 @@
-const os = require('os');
-console.log(os.networkInterfaces()['Wi-Fi'][1].address);
+const fs = require('fs');
+
+const readableStream = fs.createReadStream('./homework2.txt',{
+    highWaterMark: 1
+});
+const writeStream =fs.createWriteStream('homework2arm.txt');
+var engArm={
+        a : 'ա',
+		b : 'բ',
+		g : 'գ',
+		d : 'դ',
+		e : 'ե',
+		z : 'զ',
+		e : 'է',
+		y : 'ը',
+		t : 'թ',
+		i : 'ի',
+		l : 'լ',
+		x : 'խ',
+		k : 'կ',
+		h : 'հ',
+		m : 'մ',
+		y : 'յ',
+		n : 'ն',
+		o : 'ո',
+		p : 'պ',
+		j : 'ջ',
+		r : 'ռ',
+		s : 'ս',
+		v : 'վ',
+		t : 'տ',
+		r : 'ր',
+		c : 'ց',
+		w : 'ւ',
+		p : 'փ',
+		q : 'ք',
+		o : 'օ',
+		f : 'ֆ',
+		u : 'ու',
+		
+		}
+
+readableStream.setEncoding('UTF8')
+
+readableStream.on('data', (chunk) => {
+	var index=chunk.toString().toLowerCase()
+	if(engArm[index]==undefined){
+		writeStream.write(' ')
+	}else{
+		
+		writeStream.write(engArm[index])
+	}
+   
+ 
+});
+
+readableStream.on('end', () => {
+    console.log('DONE');
+});
+
+
+  
