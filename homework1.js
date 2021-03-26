15c7c2c1e9d86d91b0c6d3edc474e20866f38228
Ani <anihambardzumyan97@gmail.com>
2021-03-26 16:00:24 +0400

    updated

--- homework1.js
+++ homework1.js
@@ -1,14 +1,8 @@
-var http = require('http');
+var events = require('events');
+var fs=require("fs")
+var myEmitter = new events.EventEmitter();
 
-// Create a server
-http.createServer( function (req, res) { 
-
-   if(req.url=='/sunny'){
-     console.log('Yes');
-   }
-   
- 
-}).listen(3000);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:3000/')
+myEmitter.on('Homework', () => {
+ console.log(fs.readFileSync("Homeworks.txt","UTF-8"));
+});
+myEmitter.emit('Homework');
